383dc052b20fb8caad7dcbd3d311ba76bfbd9dc2
Liam McCartney <liam.mccartney@gmail.com>
2024-06-02 16:09:03 -0400

    more configurable jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,19 +2,50 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
-@onready var sprite = $AnimatedSprite2D
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var sprite = $AnimatedSprite2D
+
+@export_group("Jump")
+@export var jump_height_px = 50
+@export var jump_time_up = 0.6
+@export var jump_time_down = 0.3
+
+@onready var jump_velocity = -(2.0 * jump_height_px) / jump_time_up
+@onready var jump_gravity = (2.0 * jump_height_px) / (jump_time_up * jump_time_up)
+@onready var fall_gravity = (2.0 * jump_height_px) / (jump_time_down * jump_time_down)
+
+
+var gravity: float:
+	get:
+		return jump_gravity if velocity.y < 0.0 else fall_gravity 
+func _ready():
+	print("Jump Height:", jump_height_px)
+	print("Jump t Peak:", jump_time_up)
+	print("Jump t Descent:", jump_time_down)
+	print("Jump Velocity:", round(jump_velocity))
+	print("Jump Gravity:", round(jump_gravity))
+	print("Fall Gravity:", round(fall_gravity))
+
+func get_horizontal_velocity() -> float:
+	var horizontal := 0.0
+	
+	if Input.is_action_pressed("move_left"):
+		horizontal -= 1.0
+	if Input.is_action_pressed("move_right"):
+		horizontal += 1.0
+	
+	return horizontal
+
+func jump():
+	velocity.y = jump_velocity
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,9 +64,6 @@ func _physics_process(delta):
 	else:
 		sprite.play("jump")
 		
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = get_horizontal_velocity() * SPEED
 
 	move_and_slide()
